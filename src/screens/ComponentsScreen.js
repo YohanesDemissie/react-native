708efafb834263fc4a5cc0ac7fc63ce670e10d04
Yohanes Demissie <Yohanesdemissie@gmail.com>
2019-11-18 23:44:27 -0800

    setting stage for component screens before creating ui

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -4,7 +4,7 @@ import { Text, StyleSheet } from 'react-native';
 export function ComponentsScreen() {
   return(
     <div>
-      
+      This will be where our component files go!
     </div>
   )
 }
